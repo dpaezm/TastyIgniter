1d856dda0b848882f84302fb8fb0524b24c8abf8
Diego Páez <diegopaezmacias@gmail.com>
2025-07-20 11:16:28 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -e
 
+echo "--- Limpiando cachés... ---"
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
+# Creamos migraciones y seeds usando drivers seguros (file)
+echo "--- Ejecutando migraciones y seeds... ---"
+CACHE_DRIVER=file SESSION_DRIVER=file php artisan migrate --force --seed
+
 php artisan storage:link
 
-echo "--- Arrancando servidor en producción ---"
+# Iniciamos app temporalmente en puerto 3000
 exec php artisan serve --host=0.0.0.0 --port=3000

700ccbeff4046360d5c2c4d8d244b2d5e2848271
Diego Páez <diegopaezmacias@gmail.com>
2025-07-20 14:36:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,24 +8,23 @@ if [ ! -f /var/www/html/.env ]; then
 fi
 
 echo "--- INICIANDO ENTRYPOINT SCRIPT ---"
-
 echo "--- Limpiando cachés... ---"
 CACHE_DRIVER=file php artisan config:clear
 CACHE_DRIVER=file php artisan route:clear
 CACHE_DRIVER=file php artisan view:clear
 
-# Control de primera instalación
-INSTALL_FLAG="/app/.env.installed"
+# ✅ Flag correcto
+INSTALL_FLAG="/var/www/html/.env.installed"
 
 if [ ! -f "$INSTALL_FLAG" ]; then
   echo "--- Primera ejecución detectada. Ejecutando instalación completa... ---"
   php artisan igniter:install --no-interaction
-  php artisan storage:link
+  [ ! -L /var/www/html/public/storage ] && php artisan storage:link
   touch "$INSTALL_FLAG"
 else
   echo "--- Sistema ya instalado previamente. Ejecutando migraciones... ---"
   php artisan migrate --force
-  php artisan storage:link
+  [ ! -L /var/www/html/public/storage ] && php artisan storage:link
 fi
 
 echo "--- Arrancando PHP-FPM ---"

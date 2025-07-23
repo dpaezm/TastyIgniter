fcc4d6db773e96ee7d9d299af20cd86582a3ff75
dpaezm <diegopaezmacias@gmail.com>
2025-07-23 22:33:31 +0200

    d

--- themes/tastyigniter-orange/webpack.mix.js
+++ themes/tastyigniter-orange/webpack.mix.js
@@ -1,6 +1,3 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix.setPublicPath('public') // esto es clave
-   .sass('resources/assets/sass/main.scss', 'assets/css')
-   .js('resources/assets/js/app.js', 'assets/js')
-   .version();
+mix.setPublicPath("public").version();

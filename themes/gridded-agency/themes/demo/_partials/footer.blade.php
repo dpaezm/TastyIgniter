d6467033491cb4340e987f818b5813e799f8e35d
dpaezm <diegopaezmacias@gmail.com>
2025-07-24 07:48:33 +0200

    m

---
+++ themes/gridded-agency/themes/demo/_partials/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="container text-center">
+    <p class="small mb-0 p-3">
+        {!! sprintf(
+            lang('igniter::main.site_copyright'),
+            date('Y'),
+            setting('site_name'),
+            lang('igniter::system.system_name')
+        ).lang('igniter::system.system_powered') !!}
+    </p>
+</div>

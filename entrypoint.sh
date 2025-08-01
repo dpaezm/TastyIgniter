c9ea2c38ea63578086c8259ecd9f1f047c3ded48
Diego Páez <diegopaezmacias@gmail.com>
2025-08-01 19:08:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,10 @@ env | grep -E '^(APP_|DB_|CACHE_|SESSION_|TI_THEME)'
 
 cd /var/www/html
 
+# Asegurar carpetas necesarias para Laravel
+mkdir -p storage/framework/{cache/data,sessions,views} && chown -R www-data:www-data storage
+
+
 # Crear .env si no existe
 if [ ! -f ".env" ]; then
   echo "--- .env no encontrado, creando uno nuevo ---"

cadfb91111d1854470ccb271aa64c24d13c43d6e
dpaezm <diegopaezmacias@gmail.com>
2025-08-06 14:44:54 +0200

    instalar api reservations v8

--- extensions/Gridded/ApiReservationExtension/src/Extension.php
+++ extensions/Gridded/ApiReservationExtension/src/Extension.php
@@ -2,17 +2,22 @@
 
 namespace Gridded\ApiReservationExtension;
 
-use Igniter\System\Classes\BaseExtension;      // ⬅️  CORREGIDO
-use Igniter\System\Classes\RouteRegistrar;     // ⬅️  CORREGIDO
+use Igniter\System\Classes\BaseExtension;
+use Igniter\System\Classes\RouteRegistrar;
+
 
 class Extension extends BaseExtension
 {
-    public function register() {}
-    public function boot() {}
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
 
     public function registerRoutes()
     {
-        RouteRegistrar::instance()
-            ->registerRoutesFromFile(__DIR__.'/routes.php');
+        RouteRegistrar::instance()->registerRoutesFromFile(__DIR__.'/routes.php');
     }
 }

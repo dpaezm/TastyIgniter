d1ee4b7fdeb2e72b2fe1a62d666771dc8565f7b0
Diego Páez <diegopaezmacias@gmail.com>
2025-08-06 08:15:07 +0200

    Update routes.php

--- extensions/Gridded/ApiReservationExtension/src/routes.php
+++ extensions/Gridded/ApiReservationExtension/src/routes.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Gridded\ApiReservationExtension\Http\Controllers\ApiReservations;
 
 Route::prefix('api/reservations')->group(function () {
-    Route::post('/create', [ApiReservationController::class, 'store']);
+    Route::post('/create', [ApiReservations::class, 'store']);
 });
